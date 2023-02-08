961ed9c9ea6594638f49e5b237cb1522f4e7336d
Kuat Zharykbayev <116493714+Kuatella@users.noreply.github.com>
2023-02-09 00:30:25 +0600

    Create python_classes_1.py

---
+++ tsis3/python_classes_1.py
@@ -0,0 +1,13 @@
+#1
+class StringClass:
+    def __init__(self):
+        self.string = input()
+
+    def getString():
+        string = str(input())
+
+    def printString(self):
+        print(self.string.upper())
+        
+sss = StringClass()
+sss.printString()
